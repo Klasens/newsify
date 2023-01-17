0c3e1e1839612bf572734b5e9fcbe1df8725251e
Bob Klasen <robfklasen@gmail.com>
2023-01-17 13:41:14 -0600

    added articles into local database

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the serverside!', app: 'Express' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the serverside!', app: 'Express' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+const articles = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/articles.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+app.get('/api/v1/articles', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: articles.articles.length,
+    data: {
+      articles,
+    },
+  });
 });
 
 const port = 3000;
