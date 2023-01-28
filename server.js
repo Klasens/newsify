7292fe4ae6320c70b80c0c4e362d937acb58be4c
Klasens <57231452+Klasens@users.noreply.github.com>
2023-01-28 17:41:06 -0600

    updating server and package.json

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose
   });
 
 //* ======= Server ======= //
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
