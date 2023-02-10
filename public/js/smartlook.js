0f508f6fe49f7dbf853cdc740b662afb0ed2a797
Klasens <57231452+Klasens@users.noreply.github.com>
2023-02-09 19:29:25 -0600

    adding in tracking

---
+++ public/js/smartlook.js
@@ -0,0 +1,17 @@
+window.smartlook ||
+  (function (d) {
+    var o = (smartlook = function () {
+        o.api.push(arguments);
+      }),
+      h = d.getElementsByTagName('head')[0];
+    var c = d.createElement('script');
+    o.api = new Array();
+    c.async = true;
+    c.type = 'text/javascript';
+    c.charset = 'utf-8';
+    c.src = 'https://web-sdk.smartlook.com/recorder.js';
+    h.appendChild(c);
+  })(document);
+smartlook('init', '97ea8a52a438536996b7d2d806a21c68054dcd40', {
+  region: 'eu',
+});
